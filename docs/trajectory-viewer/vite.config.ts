03935c302dacd07f8aeaee9a98e566480823da5b
Noah Gundotra <noah@gundotra.org>
2025-08-19 16:43:56 -0400

    fix base

--- docs/trajectory-viewer/vite.config.ts
+++ docs/trajectory-viewer/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/solana-gym-env/trajectory-viewer/',
+  base: '/solana-gym-env/trajectory-viewer/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
